435c87d9fca4409ff1cacd92053c6e6b2635c8f2
Charles Perkins <charlesap@gmail.com>
2019-11-14 19:58:25 -0800

    update risc boot loader path

--- test-risc6.sh
+++ test-risc6.sh
@@ -1,2 +1,2 @@
-./risc6-softmmu/qemu-system-risc6 -machine fpga-risc -vga cg3 -g 1024x768x8 -monitor stdio -device loader,file=risc-boot.bin,addr=0xffffe000 -S
+./risc6-softmmu/qemu-system-risc6 -machine fpga-risc -vga cg3 -g 1024x768x8 -monitor stdio -device loader,file=hw/risc6/risc-boot.bin,addr=0xffffe000 -S
  
