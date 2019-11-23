b0e46b3dbab202b2a202c662f175764034c9856e
Charles Perkins <charlesap@gmail.com>
2019-11-23 10:50:43 -0800

    make all register names 2 letters

--- disas/risc6.h
+++ disas/risc6.h
@@ -1,11 +1,11 @@
 #include "qemu/bitops.h"
 
 static const char * const regnames[] = {
-    "r0 ",         "r1 ",         "r2 ",        "r3 ",
-    "r4 ",         "r5 ",         "r6 ",        "r7 ",
-    "r8 ",         "r9 ",         "r10",        "r11",
-    "mt ",         "sb ",         "sp ",        "lnk",
-    "flg",         "H  ",         "spc",        "pc "
+    "r0",         "r1",         "r2",        "r3",
+    "r4",         "r5",         "r6",        "r7",
+    "r8",         "r9",         "ra",        "rb",
+    "mt",         "sb",         "sp",        "lr",
+    "fl",         "rh",         "xc",        "pc"
 };
 
 enum {
