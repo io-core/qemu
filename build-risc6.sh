80623ca73cb48d8f327e1c3b942b094479651efa
Charles Perkins <charlesap@gmail.com>
2020-12-24 07:04:12 -0800

    build aarch64

--- build-risc6.sh
+++ build-risc6.sh
@@ -1,1 +1,1 @@
-./configure '--enable-system' '--enable-modules' '--enable-gtk' '--audio-drv-list=pa' '--enable-kvm' '--target-list=risc6-softmmu'
+./configure '--enable-system' '--enable-modules' '--enable-gtk' '--audio-drv-list=pa' '--enable-kvm' '--enable-virtfs' '--target-list=risc6-softmmu,arm-softmmu,aarch64-softmmu'
